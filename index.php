e099fd1e82fa75a1984b7d13a328b5281278e18d
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-06-21 13:45:50 +0200

    add table

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
-<?php
+    <?php
 
 $hotels = [
 
@@ -48,29 +46,91 @@ $hotels = [
 
 ];
 
-echo "<ul>";
-    foreach ($hotels as $hotel) {
-        echo "<li>";
-            echo "Name: " . $hotel["name"];
-        echo "</li>";
-        echo "<li>";
-            echo "Description: " . $hotel["description"];
-        echo "</li>";
-        echo "<li>";
-            echo "Parking: " . $hotel["parking"];
-        echo "</li>";
-        echo "<li>";
-            echo "Vote: " . $hotel["vote"];
-        echo "</li>";
-        echo "<li>";
-            echo "Distance to center: " . $hotel["distance_to_center"];
-        echo "</li>";
+$parking = $_GET['parking'];
 
-        echo "<br />";
-    }
-echo "</ul>";
+$vote = $_GET['vote'] ?? -1;
 
 ?>
+</head>
+<body>
+
+<form>
+<label for="Parcheggio">Parcheggio</label>
+<br />
+<input type="radio" id="parking_yes" name="parking" value="yes">
+<label for="parking_yes">Yes</label>
+<input type="radio" id="parking_no" name="parking" value="no">
+<label for="parking_no">No</label>
+<br />
+<label for="vote">Vote</label>
+<select name="vote" id="vote">
+    <option value="-1">SELEZIONA VOTO MINIMO</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>    
+</select>
+<br>
+<input type="submit" value="SEARCH">
+</form>
+
+<table border="1px">
+    <tr>
+        <th>NAME</th>
+        <th>DESCRIPTION</th>
+        <th>PARKING</th>
+        <th>VOTE</th>
+        <th>DISTANCE TO CENTER</th>
+    </tr>
+
+    <?php
+
+        foreach ($hotels as $hotel) {
+
+            if (($parking === null  
+                || ($parking === "yes" && $hotel['parking'])
+                || ($parking === "no" && !$hotel['parking']))
+                && $vote <= $hotel['vote']) { 
+
+                echo '<tr>';
+                echo '<td>' . $hotel['name'] . '</td>';
+                echo '<td>' . $hotel['description'] . '</td>';
+                echo '<td>' . ($hotel['parking'] ? "yes" : "no") . '</td>';
+                echo '<td>' . $hotel['vote'] . '/5</td>';
+                echo '<td>' . $hotel['distance_to_center'] . 'Km</td>';
+                echo '</tr>';
+            }
+        }
+    ?>
+</table>
+
+
+
+<?php
+// echo "<ul>";
+//     foreach ($hotels as $hotel) {
+//         echo "<li>";
+//             echo "Name: " . $hotel["name"];
+//         echo "</li>";
+//         echo "<li>";
+//             echo "Description: " . $hotel["description"];
+//         echo "</li>";
+//         echo "<li>";
+//             echo "Parking: " . $hotel["parking"];
+//         echo "</li>";
+//         echo "<li>";
+//             echo "Vote: " . $hotel["vote"];
+//         echo "</li>";
+//         echo "<li>";
+//             echo "Distance to center: " . $hotel["distance_to_center"];
+//         echo "</li>";
+
+//         echo "<br />";
+//     }
+// echo "</ul>";
+
+?>
+
 </body>
 </html>
-
